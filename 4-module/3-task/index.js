41a90fdf9f226b155a0c2ec8e4e01ca0a0e3d4e6
Oleg Obruchkov <obruchkov.o@gmail.com>
2025-08-06 21:08:33 +0300

    Решил задачи из модуля 3 и 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  const bodyRows = table.tBodies[0];
+
+  for (let i = 0; i < bodyRows.rows.length; i++) {
+    const row = bodyRows.rows[i];
+    const statusCell = row.cells[3];
+    const status = statusCell.getAttribute("data-available");
+    const genderCell = row.cells[2];
+    const ageCell = row.cells[1];
+
+    if (status === "true") {
+      row.classList.add("available");
+    } else if (status === "false") {
+      row.classList.add("unavailable");
+    } else {
+      row.hidden = true;
+    }
+
+    row.classList.add(genderCell.textContent === "m" ? "male" : "female");
+
+    if (Number(ageCell.textContent) < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
