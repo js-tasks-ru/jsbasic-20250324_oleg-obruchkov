1c2e085f868a0c94b56c60b8e88275321a2ea8c9
Oleg Obruchkov <obruchkov.o@gmail.com>
2026-01-21 22:52:56 +0300

    Решил задачи из модуля 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  let div = document.querySelector("#text");
+  button.addEventListener("click", () => {
+    div.hasAttribute("hidden")
+      ? div.removeAttribute("hidden")
+      : div.setAttribute("hidden", "");
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  const inner = document.querySelector(".carousel__inner");
+  const arrowRight = document.querySelector(".carousel__arrow_right");
+  const arrowLeft = document.querySelector(".carousel__arrow_left");
+  const slide = document.querySelector(".carousel__slide");
+  const slideWidth = slide.offsetWidth;
+  const slidesCount = 4;
+  let currentIndex = 0;
+
+  updateCarousel();
+
+  function updateCarousel() {
+    inner.style.transform = `translateX(${slideWidth * -currentIndex}px)`;
+    arrowRight.style.display = "";
+    arrowLeft.style.display = "";
+    if (currentIndex === 0) arrowLeft.style.display = "none";
+    if (currentIndex === slidesCount - 1) arrowRight.style.display = "none";
+  }
+
+  arrowRight.addEventListener("click", () => {
+    if (currentIndex < slidesCount - 1) currentIndex++;
+    updateCarousel();
+  });
+
+  arrowLeft.addEventListener("click", () => {
+    if (currentIndex > 0) currentIndex--;
+    updateCarousel();
+  });
 }
